7449d2399c063a01283ec06a1e93242ad52d7284
Matt Insler <matt.insler@gmail.com>
2013-09-06 01:22:03 -0700

    initial version

---
+++ dist/mongodb-shell-layer.js
@@ -0,0 +1,60 @@
+(function() {
+  var exec, mongodb_shell;
+
+  exec = require('child_process').exec;
+
+  module.exports = function(app) {
+    app.commandline.commands['mongodb'] = mongodb_shell.bind(app);
+    app.commandline.commands['mongodb'].help = 'mongodb [connection]';
+    return app.commandline.commands['mongodb'].description = 'Open a mongodb shell to your configured database\n  (uses the config.mongodb.url field)';
+  };
+
+  mongodb_shell = function(connection, callback) {
+    var betturl,
+      _this = this;
+    betturl = require('betturl');
+    if (typeof connection === 'function') {
+      callback = connection;
+      connection = null;
+    }
+    return this.execute('init', function() {
+      var command, config, parsed, proc, _ref, _ref1;
+      config = _this.config.mongodb;
+      if (config == null) {
+        return callback(new Error('To access the mongodb shell, you must have a mongodb configuration'));
+      }
+      if (connection != null) {
+        config = config[connection];
+        if (config == null) {
+          return callback(new Error('Could not find the ' + connection + ' connection in your mongodb configuration'));
+        }
+      }
+      parsed = betturl.parse(config.url);
+      command = 'mongo';
+      if ((((_ref = parsed.auth) != null ? _ref.user : void 0) != null) && (((_ref1 = parsed.auth) != null ? _ref1.password : void 0) != null)) {
+        command += " -u " + parsed.auth.user + " -p " + parsed.auth.password;
+      }
+      command += (" " + parsed.host) + (parsed.port != null ? ':' + parsed.port : '') + (parsed.path || '');
+      switch (require('os').type()) {
+        case 'Darwin':
+          command = 'script -q /dev/null ' + command;
+          break;
+        case 'Linux':
+          command = 'script -c "' + command + '" /dev/null';
+      }
+      proc = exec(command);
+      process.stdin.setRawMode(true);
+      process.stdin.resume();
+      proc.stdout.pipe(process.stdout);
+      proc.stderr.pipe(process.stderr);
+      process.stdin.pipe(proc.stdin);
+      proc.on('exit', function() {
+        return callback();
+      });
+      return process.on('exit', function() {
+        return proc.kill('SIGKILL');
+      });
+    });
+  };
+
+}).call(this);
---
+++ lib/mongodb-shell-layer.coffee
@@ -0,0 +1,42 @@
+{exec} = require 'child_process'
+
+module.exports = (app) ->
+  app.commandline.commands['mongodb'] = mongodb_shell.bind(app)
+  app.commandline.commands['mongodb'].help = 'mongodb [connection]'
+  app.commandline.commands['mongodb'].description = 'Open a mongodb shell to your configured database\n  (uses the config.mongodb.url field)'
+
+mongodb_shell = (connection, callback) ->
+  betturl = require 'betturl'
+  
+  if typeof connection is 'function'
+    callback = connection
+    connection = null
+  
+  @execute 'init', =>
+    config = @config.mongodb
+    return callback(new Error('To access the mongodb shell, you must have a mongodb configuration')) unless config?
+    
+    if connection?
+      config = config[connection]
+      return callback(new Error('Could not find the ' + connection + ' connection in your mongodb configuration')) unless config?
+    
+    parsed = betturl.parse(config.url)
+    
+    command = 'mongo'
+    command += " -u #{parsed.auth.user} -p #{parsed.auth.password}" if parsed.auth?.user? and parsed.auth?.password?
+    command += " #{parsed.host}" + (if parsed.port? then ':' + parsed.port else '') + (parsed.path or '')
+    
+    switch require('os').type()
+      when 'Darwin' then command = 'script -q /dev/null ' + command
+      when 'Linux' then command = 'script -c "' + command + '" /dev/null'
+    proc = exec(command)
+    
+    process.stdin.setRawMode(true)
+    process.stdin.resume()
+    
+    proc.stdout.pipe(process.stdout)
+    proc.stderr.pipe(process.stderr)
+    process.stdin.pipe(proc.stdin)
+    
+    proc.on 'exit', -> callback()
+    process.on 'exit', -> proc.kill('SIGKILL')
